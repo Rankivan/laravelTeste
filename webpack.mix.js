218eb052616af27695c4941b352fdbf1fbb99fe7
Rankivan <ivanrank@yahoo.com.br>
2021-10-25 22:37:41 -0300

    Feat: melhorias la url

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,10 +1,10 @@
 const mix = require('laravel-mix');
 
     //Webpack de arquivos JS
-    mix.js('resources/views/js/global/global.js', 'public/assets/appJs/app.js');
-    mix.js('resources/views/js/usuario/form.js', 'public/assets/appJs/app.js');
-    mix.js('resources/views/js/usuario/index.js', 'public/assets/appJs/app.js');
+    mix.js('resources/views/js/global/global.js', 'assets/appJs/app.js');
+    mix.js('resources/views/js/usuario/form.js', 'assets/appJs/app.js');
+    mix.js('resources/views/js/usuario/index.js', 'assets/appJs/app.js');
 
     //Webpack de arquivos CSS
-    mix.css('resources/views/css/global/loadding.css',     'public/assets/appCss/app.css');
+    mix.css('resources/views/css/global/loadding.css',     'assets/appCss/app.css');
 
